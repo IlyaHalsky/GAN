fc0972de3ccbcd301c1ac24ef45c9f715601e28b
Ilya Kachalsky <21011996@mail.ru>
2018-03-26 19:59:37 +0300

    gifs

--- utils.py
+++ utils.py
@@ -35,20 +35,3 @@ def imsave(images, size, path):
 
 def inverse_transform(images):
     return (images + 1.) / 2.
-
-def make_gif(images, fname, duration=2, true_image=False):
-    import moviepy.editor as mpy
-
-    def make_frame(t):
-        try:
-            x = images[int(len(images) / duration * t)]
-        except:
-            x = images[-1]
-
-        if true_image:
-            return x.astype(np.uint8)
-        else:
-            return ((x + 1) / 2 * 255).astype(np.uint8)
-
-    clip = mpy.VideoClip(make_frame, duration=duration)
-    clip.write_gif(fname, fps=len(images) / duration)
