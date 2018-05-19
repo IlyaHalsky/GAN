8dceeb531a8f1eb3e66f3f7d933b583d207f3f34
Ilya Kachalsky <21011996@mail.ru>
2018-05-19 16:36:21 +0300

    Cards

--- torch/model.py
+++ torch/model.py
@@ -12,7 +12,6 @@ def deconv(c_in, c_out, k_size, stride=2, pad=1, bn=True):
 
 
 class Generator(nn.Module):
-    """Generator containing 7 deconvolutional layers."""
     def __init__(self, z_dim=256, image_size=128, conv_dim=64):
         super(Generator, self).__init__()
         self.fc = deconv(z_dim, conv_dim*8, int(image_size/16), 1, 0, bn=False)
@@ -22,7 +21,7 @@ class Generator(nn.Module):
         self.deconv4 = deconv(conv_dim, 3, 4, bn=False)
         
     def forward(self, z):
-        z = z.view(z.size(0), z.size(1), 1, 1)      # If image_size is 64, output shape is as below.
+        z = z.view(z.size(0), z.size(1), 1, 1)
         out = self.fc(z)                            # (?, 512, 4, 4)
         out = F.relu(self.deconv1(out))  # (?, 256, 8, 8)
         out = F.relu(self.deconv2(out))  # (?, 128, 16, 16)
@@ -32,7 +31,6 @@ class Generator(nn.Module):
     
     
 def conv(c_in, c_out, k_size, stride=2, pad=1, bn=True):
-    """Custom convolutional layer for simplicity."""
     layers = []
     layers.append(nn.Conv2d(c_in, c_out, k_size, stride, pad))
     if bn:
@@ -41,7 +39,6 @@ def conv(c_in, c_out, k_size, stride=2, pad=1, bn=True):
 
 
 class Discriminator(nn.Module):
-    """Discriminator containing 4 convolutional layers."""
     def __init__(self, image_size=128, conv_dim=64):
         super(Discriminator, self).__init__()
         self.conv1 = conv(3, conv_dim, 4, bn=False)
@@ -50,7 +47,7 @@ class Discriminator(nn.Module):
         self.conv4 = conv(conv_dim*4, conv_dim*8, 4)
         self.fc = conv(conv_dim*8, 1, int(image_size/16), 1, 0, False)
         
-    def forward(self, x):                         # If image_size is 64, output shape is as below.
+    def forward(self, x):
         out = F.relu(self.conv1(x))    # (?, 64, 32, 32)
         out = F.relu(self.conv2(out))  # (?, 128, 16, 16)
         out = F.relu(self.conv3(out))  # (?, 256, 8, 8)
